9b06a7907f627bbb871b155ddeb40bb85a391f7e
tox303 <36279834+tox303@users.noreply.github.com>
2022-02-09 03:04:05 +0100

    Elfriede

--- plugin.audio.i_love_hirschmilch/default.py
+++ plugin.audio.i_love_hirschmilch/default.py
@@ -13,42 +13,42 @@ pluginHandle = int(sys.argv[1])
 
 name = "[B][COLOR antiquewhite]I [COLOR darkred] Love [COLOR antiquewhite] geschmeidigen Chillout [/B][/COLOR]"
 url = "https://bit.ly/2LPzJno"
-iconimage = os.path.join(addon_path,"chillicon.png")
+iconimage = os.path.join(addon_path,"media/chillicon.png")
 liz=xbmcgui.ListItem(name, iconImage=iconimage,thumbnailImage=iconimage)
 liz.setInfo( type="audio", infoLabels={ "Title": name } )
 xbmcplugin.addDirectoryItem(pluginHandle, url, liz)
 
 name = "[B][COLOR antiquewhite]I [COLOR darkred] Love [COLOR antiquewhite] kernigen Electro [/B][/COLOR]"
 url = "https://bit.ly/35hm7IZ"
-iconimage = os.path.join(addon_path,"electroicon.png")
+iconimage = os.path.join(addon_path,"media/electroicon.png")
 liz=xbmcgui.ListItem(name, iconImage=iconimage,thumbnailImage=iconimage)
 liz.setInfo( type="audio", infoLabels={ "Title": name } )
 xbmcplugin.addDirectoryItem(pluginHandle, url, liz)
 
 name = "[B][COLOR antiquewhite]I [COLOR darkred] Love [COLOR antiquewhite] Prog - House [/B][/COLOR]"
 url = "https://bit.ly/35h33uz"
-iconimage = os.path.join(addon_path,"proghouseicon.png")
+iconimage = os.path.join(addon_path,"media/proghouseicon.png")
 liz=xbmcgui.ListItem(name, iconImage=iconimage,thumbnailImage=iconimage)
 liz.setInfo( type="audio", infoLabels={ "Title": name } )
 xbmcplugin.addDirectoryItem(pluginHandle, url, liz)
 
 name = "[B][COLOR antiquewhite]I [COLOR darkred] Love [COLOR antiquewhite] Progressive Itsche [/B][/COLOR]"
 url = "https://bit.ly/2IppnIG"
-iconimage = os.path.join(addon_path,"progressiveicon.png")
+iconimage = os.path.join(addon_path,"media/progressiveicon.png")
 liz=xbmcgui.ListItem(name, iconImage=iconimage,thumbnailImage=iconimage)
 liz.setInfo( type="audio", infoLabels={ "Title": name } )
 xbmcplugin.addDirectoryItem(pluginHandle, url, liz)
 
 name = "[B][COLOR antiquewhite]I [COLOR darkred] Love [COLOR antiquewhite] Psytrance und UmpaLumPa [/B][/COLOR]"
 url = "https://bit.ly/2njb4y1"
-iconimage = os.path.join(addon_path,"psyicon.png")
+iconimage = os.path.join(addon_path,"media/psyicon.png")
 liz=xbmcgui.ListItem(name, iconImage=iconimage,thumbnailImage=iconimage)
 liz.setInfo( type="audio", infoLabels={ "Title": name } )
 xbmcplugin.addDirectoryItem(pluginHandle, url, liz)
 
 name = "[B][COLOR antiquewhite]I [COLOR darkred] Love [COLOR antiquewhite] Fettich geölten Techno [/B][/COLOR]"
 url = "https://bit.ly/3acMXp8"
-iconimage = os.path.join(addon_path,"technoicon.png")
+iconimage = os.path.join(addon_path,"media/technoicon.png")
 liz=xbmcgui.ListItem(name, iconImage=iconimage,thumbnailImage=iconimage)
 liz.setInfo( type="audio", infoLabels={ "Title": name } )
 xbmcplugin.addDirectoryItem(pluginHandle, url, liz)
